8efd4300ae3f1c3ff44608bd5b85552b589bbadd
Roché Compaan <roche@upfrontsystems.co.za>
2011-08-06 21:20:24 +0200

    make drawgraph function for reuse by tinymce plugin and document views

--- fullmarks/tinymceplugins/asciisvg/skins/tinymce_plugin_asciisvg/asciisvg/js/asciisvg.js
+++ fullmarks/tinymceplugins/asciisvg/skins/tinymce_plugin_asciisvg/asciisvg/js/asciisvg.js
@@ -1,14 +1,13 @@
-$(function() {
-    $('span.ASCIISvgScript').each(function(index, element) {
-        script = element.innerHTML;
-        // strip away comment tags
-        script = script.slice(11, -5);
-        console.log(script);
-        // translate arrow markers to something ASCIIMathML expects
-        script = script.replace('^-', '<-', 'g');
-        script = script.replace('-^', '->', 'g');
-        picture = element.nextSibling;
-        initialized = false;
-        translateandeval(script);
-    });
-});
+function drawgraph(svgscript) {
+    script = svgscript.innerHTML;
+    // strip away comment tags
+    script = script.slice(11, -5);
+
+    // translate arrow markers to something ASCIIMathML expects
+    script = script.replace('^-', '<-', 'g');
+    script = script.replace('-^', '->', 'g');
+    picture = svgscript.nextSibling;
+    initialized = false;
+    translateandeval(script);
+}
+
