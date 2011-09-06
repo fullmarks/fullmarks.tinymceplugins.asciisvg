4b5d19fc96be72b235a4eaf55e717777d14a2fbb
Roché Compaan <roche@upfrontsystems.co.za>
2011-09-06 19:59:22 +0200

    use regexp syntax for string replacement since the global match flag ('g') is not supported by all browsers

--- fullmarks/tinymceplugins/asciisvg/skins/tinymce_plugin_asciisvg/asciisvg/editor_plugin.js
+++ fullmarks/tinymceplugins/asciisvg/skins/tinymce_plugin_asciisvg/asciisvg/editor_plugin.js
@@ -48,8 +48,8 @@
                     // strip away comment tags
                     script = script.slice(11, -5);
                     // translate arrow markers to something ASCIIMathML expects
-                    script = script.replace('^-', '<-', 'g');
-                    script = script.replace('-^', '->', 'g');
+                    script = script.replace(/\^-/g, '<-');
+                    script = script.replace(/-\^/g, '->');
 					isnew = false;
 					elwidth = parseInt(ed.dom.getStyle(svggraph,"width"));
 					elheight = parseInt(ed.dom.getStyle(svggraph,"height"));
--- fullmarks/tinymceplugins/asciisvg/skins/tinymce_plugin_asciisvg/asciisvg/js/asciisvg.js
+++ fullmarks/tinymceplugins/asciisvg/skins/tinymce_plugin_asciisvg/asciisvg/js/asciisvg.js
@@ -1,13 +1,13 @@
 showcoordinates = false;
 
 function drawgraph(svgscript) {
-    script = svgscript.innerHTML;
+    var script = svgscript.innerHTML;
     // strip away comment tags
     script = script.slice(11, -5);
 
     // translate arrow markers to something ASCIIMathML expects
-    script = script.replace('^-', '<-', 'g');
-    script = script.replace('-^', '->', 'g');
+    script = script.replace(/\^-/g, '<-');
+    script = script.replace(/-\^/g, '->');
     picture = svgscript.nextSibling;
     initialized = false;
     translateandeval(script);
